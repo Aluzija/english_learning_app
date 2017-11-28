cecda496a7c77c52beaf81a194925e310b101b79
Paulina Sorys <poland_paulina@yahoo.pl>
2017-11-28 21:32:44 +0100

    action question_type_1 has required locals, method 'words' on session works

--- app/controllers/words/learning_session_controller.rb
+++ app/controllers/words/learning_session_controller.rb
@@ -2,16 +2,19 @@ class Words::LearningSessionController < ApplicationController
   before_action :delete_uncompleted, only: :create
 
   def create
-    @to_learn_words = Word.order(:id).limit(params[:how_many])
-    words_ids = Array.new(@to_learn_words.map { |word| word.id })
+    @words_to_learn = Word.order(:id).limit(params[:how_many])
+    words_ids = Array.new(@words_to_learn.map { |word| word.id })
     @session = Words::LearningSession.new(words_ids: words_ids, user_id: params[:user_id])
     @session.save
-    redirect_to question_type_1_user_packet_words_learning_session_path(id: @session.id)
+    redirect_to question_type_1_user_packet_words_learning_session_path(id: @session.id, index: 0)
   end
 
 
   def question_type_1
-    # @similar_words = Array.new(@to_learn_words.map { |word| word.similar })
+    @session = Words::LearningSession.find(params[:id])
+    @words = @session.words
+    @word = @words[params[:index].to_i]
+    @similar_words = @word.similar
   end
 
   private
--- app/models/words/learning_session.rb
+++ app/models/words/learning_session.rb
@@ -3,5 +3,16 @@ class Words::LearningSession < ApplicationRecord
   serialize :good_answers, Hash
   serialize :wrong_answers, Hash
   serialize :words_ids, Array
-  
+
+  def words
+    arr = words_ids.map do |id|
+      id.to_s
+    end
+    Word.find(arr)
+  end
+
 end
+
+# words = words_ids.map do |id|
+#   Word.find(id)
+# end
