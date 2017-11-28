053820d45f6e1052e908ee61f61d8213455910d3
Paulina Sorys <poland_paulina@yahoo.pl>
2017-11-28 20:11:49 +0100

    when creating new learning session all records where completed = false are destroyed

--- app/controllers/words/learning_session_controller.rb
+++ app/controllers/words/learning_session_controller.rb
@@ -1,4 +1,5 @@
 class Words::LearningSessionController < ApplicationController
+  before_action :delete_uncompleted, only: :create
 
   def create
     @to_learn_words = Word.order(:id).limit(params[:how_many])
@@ -8,8 +9,16 @@ class Words::LearningSessionController < ApplicationController
     redirect_to question_type_1_user_packet_words_learning_session_path(id: @session.id)
   end
 
+
   def question_type_1
     # @similar_words = Array.new(@to_learn_words.map { |word| word.similar })
+  end
+
+  private
+
+  def delete_uncompleted
+    uncomplited = Words::LearningSession.where(completed: false)
+    uncomplited.each {|u| u.destroy}
   end
 
 end
